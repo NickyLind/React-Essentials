2fb732e72f456e10a204f725505eecd719791c3d
Nick Lindau <nicholaithegreat@gmail.com>
2025-04-18 13:54:36 -0700

    Add 'dynamic' data to CoreConcept component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Cruciual", "Core"];
 function genRandomInt(max) {
@@ -24,7 +24,7 @@ function Header() {
 function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
+      <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -40,9 +40,20 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title={"Something pretty dumb"}
-              description={"Something stupid"}
-              img={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
             />
           </ul>
         </section>
