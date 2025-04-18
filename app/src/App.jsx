26c738e8bf83c0890086081c195082a87fd84125
Nick Lindau <nicholaithegreat@gmail.com>
2025-04-18 14:58:10 -0700

    create TabButton component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
   return (
@@ -27,6 +28,15 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Boner-Pills</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
